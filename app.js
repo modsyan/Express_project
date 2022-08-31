fa4277bfb4d488e79798015aa25064459bc88402
Muhammed Hamdy <modclover7@gmail.com>
2022-09-01 01:39:41 +0200

    Create Post Method

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // app.get("/", (req, res) => {
 //   // res.status(200).send("Hello from Server Side!");
 //   res.status(200).json({
@@ -26,6 +28,20 @@ app.get("/api/v1/tours/", (req, res) => {
       tours,
     },
   });
+});
+
+app.post("/api/v1/tours/", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  let newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json(newTour);
+    }
+  );
 });
 
 const port = 4000;
