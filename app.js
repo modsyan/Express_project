fba4a92e1b98e5b6a66fc7d3a89d195a962b6a30
Muhammed Hamdy <modclover7@gmail.com>
2022-09-01 00:45:42 +0200

    get handled

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  // res.status(200).send("Hello from Server Side!");
-  res.status(200).json({
-    msg: "Hello from server side!",
-    app: "first app using express js framework",
-    date: "Aug 31 2022",
-  });
-});
+// app.get("/", (req, res) => {
+//   // res.status(200).send("Hello from Server Side!");
+//   res.status(200).json({
+//     msg: "Hello from server side!",
+//     app: "first app using express js framework",
+//     date: "Aug 31 2022",
+//   });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send("you can post to this url now...");
+// app.post('/', (req, res) => {
+//   res.status(200).send("you can post to this url now...");
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 4000;
