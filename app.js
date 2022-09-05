e7cdb7dfad6397127a98a1175ddfee959c5e3d40
Muhammed Hamdy <modclover7@gmail.com>
2022-09-05 04:41:07 +0200

    Before Refactoring code

--- app.js
+++ app.js
@@ -4,19 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   // res.status(200).send("Hello from Server Side!");
-//   res.status(200).json({
-//     msg: "Hello from server side!",
-//     app: "first app using express js framework",
-//     date: "Aug 31 2022",
-//   });
-// });
-
-// app.post('/', (req, res) => {
-//   res.status(200).send("you can post to this url now...");
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -26,6 +13,27 @@ app.get("/api/v1/tours/", (req, res) => {
     status: "success",
     data: {
       tours,
+    },
+  });
+});
+
+// ? for optional parameters /api/v1/tours/:id/:name?/:age?/
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const tour = tours.find((el) => el.id == req.params.id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+      Descriptions: "out of range id.",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
     },
   });
 });
@@ -44,7 +52,65 @@ app.post("/api/v1/tours/", (req, res) => {
   );
 });
 
-const port = 4000;
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id > tours.length) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+      Descriptions: "out of range id.",
+    });
+  }
+  console.log(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Updated <SUCCESS>",
+    },
+  });
+});
+
+/** NEEDED TO MAINTAINS */
+// app.patch("/api/v1/:id", (req, res) => {
+//   // search if the id here
+//   const updatedTour = tours.find((el) => el.id == req.params.id);
+//   if (!updatedTour) {
+//     res.status(404).json({
+//       status: "fail",
+//       message: "Invalid ID",
+//       Descriptions: "out of range id.",
+//     });
+//   }
+//   tours
+//   const updateTour = res.body;
+//   fs.writeFile(
+//     `${__dirname}/dev-data/data/tours-simple.json`,
+//     JSON.stringify(tours),
+//   );
+
+//   res.status(200).json({
+//     status:"success",
+//     message:"Updated successfully",
+//     data:{
+//       res.body;
+//     }
+//   })
+// });
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id > tours.length) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+      Descriptions: "out of range id.",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+const port = 3200;
 app.listen(port, () => {
   console.log(`App Running at ${port} Port Number...`);
 });
